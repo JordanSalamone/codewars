dec57467adf4b0cd1e55699daee46dea54fc9892
Jordan Salamone <jordansalamone@me.com>
2024-06-05 07:12:39 -0500

    always returns the number 5

---
+++ 8-kyu/5-without-numbers.js
@@ -0,0 +1,17 @@
+// 8
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+function unusualFive() {
+    return 'fifth'.length;
+}
+
+
+
+// my favorite other solution:
+
+function unusualFive() {
+    return 'fucku'.length
+}
