d4efff6d861e9feebd4b76bad9dbd6a1fceb8941
Jordan Salamone <jordansalamone@me.com>
2024-07-20 23:18:58 -0500

    returns input string either all lower or upper case depending on which occurs more in og string

---
+++ 7-kyu/fix-string-case.js
@@ -0,0 +1,34 @@
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s){
+    let upper = 0,
+        lower = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s.charCodeAt(i) < 91){
+            upper += 1
+        }else{
+            lower += 1
+        }
+    }
+    return lower >= upper ? s.toLowerCase() : s.toUpperCase();
+}
+
+console.log(solve("CODe"))
+
+
+
+// other solutions
+
+function solve(s){
+    let upper = s.split('').filter(x => x === x.toUpperCase()).length
+    let lower = s.length - upper
+    return (upper > lower) ? s.toUpperCase() : s.toLowerCase() 
+  }
