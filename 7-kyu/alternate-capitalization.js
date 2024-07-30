32a0578bd75b645cfa9516857b5bd3ff1d0334e6
Jordan Salamone <jordansalamone@me.com>
2024-07-30 08:54:16 -0500

    updated

--- 7-kyu/alternate-capitalization.js
+++ 7-kyu/alternate-capitalization.js
@@ -32,4 +32,5 @@ function capitalize(s){
     const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
     const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
     return [even, odd];
+    // yep
   };
