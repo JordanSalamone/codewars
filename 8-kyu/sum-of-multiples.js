103cef57ebb0def9e8c5059d49ea8d5fb51b8f1f
Jordan Salamone <jordansalamone@me.com>
2024-08-22 13:47:44 -0500

    returns sum of all multiples between two numbers

---
+++ 8-kyu/sum-of-multiples.js
@@ -0,0 +1,26 @@
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+    let sum = 0
+    if(n < m){
+        for(let i = 0; i <= m; i += n){
+            sum += i
+        }
+        return sum
+    }else{
+        return 'INVALID'
+    }
+}
+
+console.log(sumMul(2,9))
