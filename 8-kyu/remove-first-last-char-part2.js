cd13add1ad6c61d212fffb59f3724401348afbda
Jordan Salamone <jordansalamone@me.com>
2024-09-09 14:03:51 -0500

    removes the 1st and last character and return new string or if empty returns null

---
+++ 8-kyu/remove-first-last-char-part2.js
@@ -0,0 +1,33 @@
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// Examples
+
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+    let str = string.split(',')
+    str.pop()
+    str.shift()
+    return str == '' ? null : str.join(' ')
+}
+
+array('A1,B2')
+
+
+
+
+// other solutions
+
+function array(arr){
+    return arr.split(",").slice(1,-1).join(" ") || null;
+  }
