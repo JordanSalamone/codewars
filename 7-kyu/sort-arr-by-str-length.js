f82370c4a9318b70a53665d7d5a930d0af9de184
Jordan Salamone <jordansalamone@me.com>
2024-09-11 22:48:54 -0500

    reduced lines in code

--- 7-kyu/sort-arr-by-str-length.js
+++ 7-kyu/sort-arr-by-str-length.js
@@ -11,7 +11,5 @@
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 
 function sortByLength (array) {
-    // Return an array containing the same strings,
-    // ordered from shortest to longest
     return array.sort((a,b) => a.length - b.length);
-}
+} 
