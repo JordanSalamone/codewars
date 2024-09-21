e235e2be7128223aa3944fa2ddd638b5e4be5fd0
Jordan Salamone <jordansalamone@me.com>
2024-09-21 08:49:20 -0500

    a waste of time

--- 6-kyu/tortoise-racing.js
+++ 6-kyu/tortoise-racing.js
@@ -17,11 +17,14 @@
 
 
 function race(v1, v2, g) {
-    if(v1 >= v2){
-        return null
-    }else{
-        
-    }
+    if (v2 < v1) { return null; }
+    
+    var seconds = Math.floor(g / (v2 - v1) * 3600);
+    var h = Math.floor(seconds / 3600);
+    var m = Math.floor((seconds - h * 3600) / 60);
+    var s = seconds - h * 3600 - m * 60;
+    
+    return [h, m, s];
 }
 
 
