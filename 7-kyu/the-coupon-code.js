4d5c83e4ce3ba77b0fe0f73be92c11245c768a17
Jordan Salamone <jordansalamone@me.com>
2024-09-28 21:10:09 -0500

    returns true or false if coupon code is expired or current. This taught me how to use the Date.parse() function

--- 7-kyu/the-coupon-code.js
+++ 7-kyu/the-coupon-code.js
@@ -15,22 +15,7 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
-    let curMonth = currentDate.split(' ')[0],
-        expMonth = expirationDate.split(' ')[0],
-        curDay = currentDate.split(',')[0].split(' ')[1],
-        expDay = expirationDate.split(',')[0].split(' ')[1],
-        curYear = currentDate.split(' ')[2],
-        expYear = expirationDate.split(' ')[2];
-    const m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    if(enteredCode === correctCode){
-        if(m.indexOf(curMonth) < m.indexOf(expMonth) && curYear == expYear){
-            return true
-        }else if(m.indexOf(curMonth) == m.indexOf(expMonth) && curYear == expYear){
-            return 1
-        }
-    }else{
-        return false
-    }
-}
+    return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+  }
 
 console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014'))
