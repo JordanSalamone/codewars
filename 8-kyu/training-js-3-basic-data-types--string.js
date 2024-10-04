f38695ebe568b5d17a3e61084be2d20843d03c67
Jordan Salamone <jordansalamone@me.com>
2024-10-03 21:46:43 -0500

    possibly the most aimless exersize, but we did it :)

--- 8-kyu/training-js-3-basic-data-types--string.js
+++ 8-kyu/training-js-3-basic-data-types--string.js
@@ -13,27 +13,42 @@
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
   //select some variable to combine "Dad"
-  return ?+?+?;
+  return d1+a2+d2;
 }
 function Bee(){
   //select some variable to combine "Bee"
-  return ?+?+?;
+  return b1+e2+e2;
 }
 function banana(){
   //select some variable to combine "banana"
-  return ?+?+?;
+  return b2+a2+n2+a2+n2+a2;
 }
 
 //answer some questions if you finished works above
 function answer1(){
   //the answer should be "yes" or "no"
-  return "";
+  return "yes";
 }
 function answer2(){
   //the answer should be "yes" or "no"
-  return "";
+  return "no";
 }
 function answer3(){
   //the answer should be "yes" or "no"
-  return "";
+  return "no";
 }
+
+
+
+
+// refined solution:
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+
+const Dad = () => d1 + a2 + d2
+const Bee = () => b1 + e2 + e2
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+
+const answer1 = () => "no"
+const answer2 = () => "no"
+const answer3 = () => "yes"
