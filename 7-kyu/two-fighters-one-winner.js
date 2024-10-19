54cd6feaee707cb73d352d52b6d6804bbd369010
Jordan Salamone <jordansalamone@me.com>
2024-10-18 23:48:26 -0500

    using classes created a mini combat game between two fighters and return the winner

--- 7-kyu/two-fighters-one-winner.js
+++ 7-kyu/two-fighters-one-winner.js
@@ -26,5 +26,25 @@
 
 
 function declareWinner(fighter1, fighter2, firstAttacker) {
-    return "Write your code here";
+    while (fighter1.health > 0 && fighter2.health > 0) {
+      fighter2.health -= fighter1.damagePerAttack;
+      fighter1.health -= fighter2.damagePerAttack;
+    }
+    
+    if (fighter1.health <= 0 && fighter2.health <= 0)
+      return firstAttacker;
+    else if (fighter1.health <= 0)
+      return fighter2.name;
+    else
+      return fighter1.name;
+  }
+
+  function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
 }
+
+
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"))
