57ab0138b5b5afcbf1a6759acdae467bdf54f406
Jordan Salamone <jordansalamone@me.com>
2024-11-08 23:09:41 -0600

    second iteration

--- 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.js
+++ 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.js
@@ -6,10 +6,20 @@
 // Task
 
 // Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+
 // For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
 // With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
 
 function deleteNth(arr,n){
-    // ...
+    // take the arr and find the duplicates
+    let dup = arr.filter((item, index) => arr.indexOf(item) < index)
+    console.log(dup)
+    // add those duplicates to a seperate dup arr
+    for(let i = 0; i < arr.length; i++){
+        
+    }
 }
+
+deleteNth([2, 3, 3, 7, 3, 3, 2, 2, 9], 4)
