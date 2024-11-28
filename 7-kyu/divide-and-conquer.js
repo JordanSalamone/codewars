2b77c5464fc445455a660fba139fe0cbc19d6b32
Jordan Salamone <jordansalamone@me.com>
2024-11-28 14:02:46 -0600

    takes array of integers and strings and subtracts the string numbers from the integer numbers

--- 7-kyu/divide-and-conquer.js
+++ 7-kyu/divide-and-conquer.js
@@ -3,11 +3,25 @@
 // Return as a number.
 
 function divCon(x){
-    let integers = x.filter(item => Number.isInteger(item))
-        string = x.filter(item => !Number.isInteger(item))
-    integers.reduce((a,c) => a += c, 0)
-    string.reduce((a,c) => a += +c, 0)
-
+    let integers = x.filter(item => Number.isInteger(item)).reduce((a,c) => a += c, 0)
+        string = x.filter(item => !Number.isInteger(item)).reduce((a,c) => a += +c, 0)
+    return integers - string
 }
 
-divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7])
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+
+
+
+
+
+// other solutions
+
+function divCon(x){
+    const strings = x.filter(n => typeof n === 'string')
+    const nums = x.filter(n => typeof n === 'number')
+    
+    const strSum = strings.reduce((acc, str) => acc += +str, 0)
+    const numSum = nums.reduce((acc, str) => acc += str, 0)
+  
+    return numSum - strSum
+}
