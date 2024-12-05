a86c0cf70aca16091d98c9c3b15bd5e3dce655b1
Jordan Salamone <jordansalamone@me.com>
2024-12-04 23:03:10 -0600

    this one hurt my brain, returns all the ways to divide a list of at least two elements into two non-empty parts

---
+++ 7-kyu/parts-of-a-list.js
@@ -0,0 +1,44 @@
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+// Examples of returns in different languages:
+
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or 
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+function partlist(arr) {
+    let result = [],
+        result2 = [],
+        finalResult = [];
+    for(let i = 1; i < arr.length; i++){
+        result.push(arr.slice(i, arr.length).join(' '))
+        result2.push(arr.slice(0, i).join(' '))
+        finalResult.push([result2[i-1] , result[i-1]])
+    }
+    return finalResult
+}
+
+
+console.log(partlist(["I", "wish", "I", "hadn't", "come"]))
+
+
+
+
+// other solutions
+function partlist(arr) {
+    return arr.map((v, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
+}
