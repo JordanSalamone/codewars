09979fec06d54f255d0352f3cce78beb6208c382
Jordan Salamone <jordansalamone@me.com>
2024-12-07 09:17:03 -0600

    returns a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters.

--- 6-kyu/give-me-a-diamond.js
+++ 6-kyu/give-me-a-diamond.js
@@ -27,12 +27,17 @@
 // "  *\n ***\n*****\n ***\n  *\n"
 
 function diamond(n){
-    if(n%2 == 0){
-        return null
-    }else{
-        return 
-    }
-}
+    if (n < 0 || !(n % 2)) return null; 
+    const middleIndex = Math.floor(n / 2);
+    
+    return Array.apply(null, {length: n})
+        .map((el, index) => {
+          const indentation = Math.abs(index - middleIndex);
+          const numberOfAsterisks = n - indentation * 2;
+          return Array(indentation + 1).join(' ') + Array(numberOfAsterisks + 1).join('*');
+        })
+        .join('\n') + '\n';
+  }
 
 
 console.log(diamond(3))
