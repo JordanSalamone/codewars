0c0c41aa772746c447d0e6500b51ed1c7cc79e92
Jordan Salamone <jordansalamone@me.com>
2024-12-18 09:04:29 -0600

    returns the highest average of an hourly speed? Not quite sure even how this was calculated tbh

--- 7-kyu/speed-control.js
+++ 7-kyu/speed-control.js
@@ -19,6 +19,50 @@
 
 //  (3600 * delta_distance) / s.
 
+
+
+// incorrect:
+
+// function gps(s, x) {
+//     let avg = []
+//     for(let i = 1; i < x.length; i++){
+//         avg.push(Math.floor(x[i] - x[i - 1] * 3600 / s))
+//     }
+//     console.log(avg)
+//     return Math.max(Math.abs(...avg)) < 1 ? 0 : Math.max(Math.abs(...avg))
+// }
+
+gps(12, [0.0, 0.24, 0.48, 0.72, 0.96, 1.2, 1.44, 1.68, 1.92, 2.16, 2.4])
+
+
+
+
+// other solutions
+const gps = (s, x) => {
+
+    if (x.length<=1) {
+      return 0;
+    }
+    
+    let output = [];
+    for (let i = 0; i < x.length-1; i++) { 
+      output.push((x[i+1]-x[i])*3600/s);
+    }
+    
+    return Math.max(...output);
+}
+
+
+
+
+
 function gps(s, x) {
-    let meBe = 'lol number 2'
+    let distArr = x.map((point, index) =>{
+        if(point == x[0]) return point
+        return x[index] - x[index-1]
+    });
+    if(distArr.length != 0){
+        return Math.floor((3600 * Math.max(...distArr)) / s);
+    }
+    return 0;
 }
