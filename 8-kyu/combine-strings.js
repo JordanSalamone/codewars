4554a824ae732ffa445006599de499a5b5d49c83
Jordan Salamone <jordansalamone@me.com>
2024-12-19 23:55:56 -0600

    combines two input strings with a space between to create a full name

---
+++ 8-kyu/combine-strings.js
@@ -0,0 +1,9 @@
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+function combineNames(n1, n2){
+    return `${n1} ${n2}`
+}
