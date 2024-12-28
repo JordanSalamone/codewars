4b050b3360eacb1cff87e941acc00d8ab88ed9f0
Jordan Salamone <jordansalamone@me.com>
2024-12-27 23:10:47 -0600

    finds the integer that does not match the others

--- 7-kyu/find-the-stray-number.js
+++ 7-kyu/find-the-stray-number.js
@@ -14,14 +14,30 @@ function stray(numbers) {
     for(let i = 0; i < numbers.length; i++){
         let copy = numbers.slice()
             item = copy.splice(i, 1)
-            console.log(copy)
-            for(let j = 0; j < copy.length; j++){
-                if(item !== copy[j]){
-                    result.push(item)
-                }
-            }
+        if(!copy.join().includes(item)){
+            result += item
+        }
     }
-    return result
+    return +result
 }
 
 stray([1, 2, 1, 1, 1])
+
+
+
+
+// Correct solutions
+
+function stray(numbers) {
+    var a = numbers.sort();
+    
+    if(a[0] != a[1]) {
+      return a[0]
+    } 
+    return a[a.length-1]
+}
+
+
+// &
+
+const stray = nums => nums.reduce((a, b) => a ^ b);
