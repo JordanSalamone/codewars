1dabe54bfd28853318811016c2c914f4f4148ec3
Jordan Salamone <jordansalamone@me.com>
2024-12-31 09:03:40 -0600

    counts the number of occurrences of each character and return it as a (list of tuples) in order of appearance.

--- 7-kyu/ordered-count-of-characters.js
+++ 7-kyu/ordered-count-of-characters.js
@@ -7,16 +7,35 @@
 // orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 
 const orderedCount = function (text) {
-    // iterate each letter
-    // reduce each letter to a new array
-    // concat each array to the primary array WHAT IN THE WORLD!
-    let letters = text.split('')
-        result = []
-        // Why GOD
-    for(let i = 0; i < text.length; i++){
-        if(result.includes(letters[i])){
-
-        }
+    let arr = [];
+    for (let i = 0; i < text.length; i++){
+       let k = 0;
+       for(let j = 0; j < arr.length; j++)
+          if(arr[j][0] == text[i]){
+            k++;
+          }
+        if(k == 0){
+          for(let j = i; j < text.length; j++)
+            if(text[j] == text[i])
+               k++;
+            arr.push([text[i], k]);
+         }
     }
-    return [];
+    return arr;
 }
+
+
+
+
+
+
+// // other solutions
+
+
+// const orderedCount = s =>
+//     Array.from(s.split('').reduce((m, k) => m.set(k, m.has(k) ? m.get(k) + 1 : 1), new Map()));
+
+
+// var orderedCount = function (text) {
+//     return Array.from(new Set(text)).map(c => [c, text.match(new RegExp(c, 'g')).length]);
+//   }
