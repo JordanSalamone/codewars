b9caa1c54a33ec26cf8ac30a09a91cce996f93e5
Jordan Salamone <jordansalamone@me.com>
2025-01-02 23:58:35 -0600

    practicing back ticks with joining two strings together

--- 8-kyu/ES6-string-addition.js
+++ 8-kyu/ES6-string-addition.js
@@ -9,5 +9,5 @@
 // If one of the arguments is a number your code must coerce it into being a string.
 
 function joinStrings(string1, string2){
-    // code here
+    return `${string1} ${string2}`
 }
