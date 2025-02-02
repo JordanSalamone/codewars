6ce9353b83bdaa0f0d03ed72a30558ea6d19446d
Jordan Salamone <jordansalamone@me.com>
2025-02-01 23:58:55 -0600

    returns the smallest number that could be formed from these digits, using the digits only once.

---
+++ 7-kyu/form-the-minimum.js
@@ -0,0 +1,17 @@
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+
+// Examples
+
+// [1, 3, 1] ==> 13
+// [5, 7, 5, 9, 7] ==> 579
+// [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+
+function minValue(values){
+    let result = []
+    for(let i = 0; i < values.length; i++){
+        if(!result.includes(values[i])){
+            result.push(values[i])
+        }
+    }
+    return result.sort().join()
+}
