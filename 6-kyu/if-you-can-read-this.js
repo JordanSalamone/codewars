1d3a029f23bdfd5f6bcd1514640e120086f477d4
Jordan Salamone <jordansalamone@me.com>
2025-02-04 22:06:37 -0600

    translates a string to pilots alphabet (NATO phonetic alphabet)

---
+++ 6-kyu/if-you-can-read-this.js
@@ -0,0 +1,53 @@
+// You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+// Input:
+
+// If, you can read?
+
+// Output:
+
+// India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+// Note:
+
+// There is a preloaded dictionary that you can use, named NATO. It uses uppercase keys, e.g. NATO['A'] is "Alfa". See comments in the initial code to see how to access it in your language.
+// The set of used punctuation is ,.!?.
+// Punctuation should be kept in your return string, but spaces should not.
+// Xray should not have a dash within.
+// Every word and punctuation mark should be seperated by a space ' '.
+// There should be no trailing whitespace
+
+let table = {
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliett',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'Xray',
+    'Y': 'Yankee',
+    'Z': 'Zulu',
+  }
+  
+  function toNato(words) {
+    return words.split('').filter(c => c !== ' ').map(c => table[c.toUpperCase()] || c).join(' ');
+  }
+
+toNato('Did not see that coming')
