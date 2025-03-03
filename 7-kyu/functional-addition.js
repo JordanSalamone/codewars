1eb6e76d580341443395bded0121192087e633bd
Jordan Salamone <jordansalamone@me.com>
2025-03-02 22:49:48 -0600

    when complete, this function will return another function to add two numbers together

---
+++ 7-kyu/functional-addition.js
@@ -0,0 +1,13 @@
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// var addOne = add(1);
+// addOne(3); // 4
+
+// var addThree = add(3);
+// addThree(3); // 6
+
+function add(n) {
+//   find how to use a function within a function and what in the world the question is asking
+}
