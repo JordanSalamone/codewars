4b8ac3a390f829ef1bc7e30584d72b5c8d09fe15
Jordan Salamone <jordansalamone@me.com>
2025-03-22 23:22:16 -0500

    creates an object with three objects nested within it each with 3 properties

---
+++ 8-kyu/grasshopper-create-the-rooms.js
@@ -0,0 +1,19 @@
+// You are creating an "Escape the room" game. The first step is to create a hash table ( dict in Python) called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each room being a hash table with at least 3 properties (e.g. name, description, completed).
+
+const rooms = {
+    bedroom: {
+      name: 'Bedroom',
+      description: 'For... sleeping',
+      completed: true,
+    },
+    bathroom: {
+      name: 'Bathroom',
+      description: 'This will get you clean',
+      completed: false,
+    },
+    kitchen: {
+      name: 'Kitchen',
+      description: 'Hungry? Why wait?',
+      completed: true,
+    },
+  }
