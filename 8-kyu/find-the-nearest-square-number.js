8af0189e2560f1337126b811f079882c1a08815e
Jordan Salamone <jordansalamone@me.com>
2025-03-26 21:24:46 -0500

    finds nearest square number and returns that integer

---
+++ 8-kyu/find-the-nearest-square-number.js
@@ -0,0 +1,7 @@
+// Your task is to find the nearest square number of a positive integer n. In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// For example, if n = 111, then the nearest square number equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+// If n is already a perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
