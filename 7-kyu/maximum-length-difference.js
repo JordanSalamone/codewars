be0d10c1cbadc07bc586f672931b5e0e3b41c7c8
Jordan Salamone <jordansalamone@me.com>
2025-04-01 21:55:20 -0500

    You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
    // Find max(abs(length(x) − length(y)))

--- 7-kyu/maximum-length-difference.js
+++ 7-kyu/maximum-length-difference.js
@@ -15,5 +15,8 @@
 // output: number as a string
 
 function mxdiflg(a1, a2) {
-    return Math.max(abs(length(a1) − length(a2)))
-}
+    if (a1.length === 0 || a2.length === 0) return -1
+    let l1 = a1.map(str => str.length)
+    let l2 = a2.map(str => str.length)
+    return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+  }
