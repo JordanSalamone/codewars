27c97e549c6e0488f1f264869857ec4e4bc50b23
Jordan Salamone <jordansalamone@me.com>
2025-05-05 23:31:29 -0500

    using regex, this will vaildate a username when complete

---
+++ 8-kyu/simple-validation-of-username-using-regex.js
@@ -0,0 +1,11 @@
+// Write a simple regex to validate a username. Allowed characters are:
+
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+function validateUsr(username) {
+    const res =  //regex here/.test(username)
+    return res;
+}
